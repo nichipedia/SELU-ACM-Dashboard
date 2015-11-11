57aeb663af42e8e495f59ac370ae5c7b763ca3b3
Seth Champagne <champagneSeth@gmail.com>
2015-11-11 10:38:22 -0600

    File uploading works now

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var crypto = require('crypto');
-   
-
-
+var express     = require('express')
+,   app         = express()
+,   path        = require('path')
+,   mongoose    = require('mongoose')
+,   bodyParser  = require('body-parser')
+,   crypto      = require('crypto')
+,   fs          = require('fs')
+;  
 
 
 mongoose.connection.on('open', function(ref) { 
@@ -36,12 +35,17 @@ mongoose.connect('mongodb://pawn:34erdfcv#$ERDFCV@ds053794.mongolab.com:53794/se
 
 app.use('/lib', express.static(__dirname + '/app/lib'));
 app.use('/', express.static(__dirname + '/app'));
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended    : true
+,   limit       : '50mb'
+}));
+app.use(bodyParser.json({
+    limit       : '50mb'
+}));
 
 
 app.get('/favicon.ico', function(req, res) {
-    res.sendFile(path.join(_dirname + '/favicon.ico'));
+    res.sendFile(path.join(__dirname + '/favicon.ico'));
 });
 
 app.get('/home', function (req, res) {
@@ -60,69 +64,57 @@ app.post('/api/register', function(req, res) {
     
     console.log('Post success!!');
     User.find({ email : req.body.email}, function(err, user) {
-            if(err) {
-                throw err;
-                console.log('error querying db');
-            }
-            if(user.length != 0) {
-                res.send('User already exsists');
-                console.log('User already exsists');
-            }
-        else{
-
+        if(err) {
+            console.log('error querying db');
+            throw err;
+        } else if(user.length != 0) {
+            res.send('User already exsists');
+            console.log('User already exsists');
+        } else {
             var salt = generateSalt();
-            
             var password = encrypt(req.body.password, salt);
-            
             var newUser = new User({
-
-                 firstName: req.body.firstName
-                ,lastName: req.body.lastName
-                ,password: password
-                ,email: req.body.email
-                ,salt: salt
-
-
-
+                firstName   : req.body.firstName
+            ,   lastName    : req.body.lastName
+            ,   password    : password
+            ,   email       : req.body.email
+            ,   salt        : salt
             });
 
             newUser.save(function(err) {
                 if (err) throw err;
-
                 console.log('User created!');
             });
-
 
             delete user;
 
             User.findOne( {email : req.body.email}, function(err, user) {
-
                 console.log('User created this is your unique user ID: ' + user._id);
-
-                res.send('User created ' + ' your usernaame is: ' + user.userName + ' and this is your unique user ID: ' + user._id);
-
-
+                res.send('User created ' + ' your username is: ' + user.userName + ' and this is your unique user ID: ' + user._id);
             });
-
-
         }
-
-    
-    
     });
-    
-    
+});
+
+app.post('/api/upload', function(req, res) {
+    var fileName    = __dirname + '/resumes/' + req.body.name 
+    ,   contents    = req.body.file
+    ;
+
+    fs.writeFile(fileName, contents, 'binary', function(err) {
+        if (err) console.log(err);
+        else console.log('Resume received');
+    });
+
+    res.send('Resume uploaded')
 });
 
 function encrypt(plainText, salt) {
-
     var algorithm = 'aes-256-ctr';
     var cipher = crypto.createCipher(algorithm, salt);
     var crypted = cipher.update(plainText, 'utf8', 'hex');
     crypted += cipher.final('hex');
     return crypted;
-
-
 }
 
 function decrypt(encryptedText, salt){
