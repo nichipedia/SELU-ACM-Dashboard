75e778a409990de42cf8e1813a04405ace9a35c7
Nicholas <nicholas.moran@selu.edu>
2015-11-11 19:26:58 -0600

    Updated the login endpoint it now uniquely stores pdfs, Implemented web tokens for user authentication....WE MADE IT

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var express         = require('express')
 ,   bodyParser      = require('body-parser')
 ,   crypto          = require('crypto')
 ,   fs              = require('fs')
-,   methodOverride  = require('method-override');
+,   methodOverride  = require('method-override')
+,   jwt             =require('jsonwebtoken');
 ;
 
 
@@ -55,44 +56,52 @@ app.get('/home', function (req, res) {
     console.log('Success!');
 });
 
-app.post('/api/login', function(req, res) {
+
+    app.post('/api/login', function(req, res) {
     User.findOne({ email: req.body.email}, function(err, user) {
        if(err) {
            throw err;
            console.log('error querying db');
        } 
-        var userPassword = encrypt(req.body.password, user.salt);
-        if(userPassword != user.password) {
-            res.status(403);
-            console.log('Password incoorect');
-        }
-        else {
-            res.status(200).json({ "email" : user.email, "firstName" : user.firstName, "lastName" : user.lastName, "token" });
-        }
+       if(!user) {
+           res.status(401).json({ success : false, message : 'User not found in DataBase'});
+       }
+       else if(user) {
+           var hash = generateHash(req.body.password, user.salt);
+           if(hash != user.password) {
+               res.status(409).json({ success : false, message : 'User failed authentication incorrect password'});
+           }
+           else {
+               var token = jwt.sign(user, 'superSecret', {
+                  expiresIn: 1440  
+               });
+               
+               var returnUser = ({ firstName : user.firstName, lastName : user.lastName, email : user.email, userId : user._id});
+               
+               res.status(200).json({ success : true, message : 'thankyou', user: returnUser, token : token});
+           }
+       }
     });
 });
 
 
 app.post('/api/register', function(req, res) {
     console.log('Post success!!');
-    User.find({ email : req.body.email}, function(err, user) {
+    User.findOne({ email : req.body.email}, function(err, user) {
         if(err) {
             throw err;
             console.log('error querying db');
-        } else if(user.length != 0) {
+        } else if(user) {
             res.status(200).send('User already exsists');
             console.log('User already exsists');
         } else {
             var salt = generateSalt();
-            var hash = crypto
-                    .createHash('sha256')
-                    .update(salt + req.body.password)
-                    .digest('base64');
+            var hash = generateHash(req.body.password, salt);
 
             var newUser = new User({
                 firstName   : req.body.firstName
             ,   lastName    : req.body.lastName
-            ,   password    : password
+            ,   password    : hash
             ,   email       : req.body.email
             ,   salt        : salt
             });
@@ -109,25 +118,46 @@ app.post('/api/register', function(req, res) {
 });
 
 app.post('/api/upload', function(req, res) {
-    var fileName    = __dirname + '/resumes/' + req.body.name 
-    ,   contents    = req.body.file
-    ;
+ 
+    var token = req.body.token;
+    
+    if(token) {
+        jwt.verify(token, 'superSecret', function(err, decode) {
+           if(err) {
+               res.status(403).json({ success : false, message : 'Failed to authenticate token'});
+           }
+            else {
+                var fileName    = 'resumes/' + decode.lastName + '_' + decode.firstName + '_' + decode.Id + '.pdf';
+                ,   contents    = req.body.file
+                ;
 
-    fs.writeFile(fileName, contents, 'binary', function(err) {
-        if (err) console.log(err);
-        else console.log('Resume received');
-    });
+                fs.writeFile(fileName, contents, 'binary', function(err) {
+                    if (err) console.log(err);
+                    else console.log('Resume received');
+                });
 
-    res.send('Resume uploaded')
+                res.status(201).send('Resume uploaded')
+            }
+            
+        });
+    }
+    else {
+        res.status(403).json({ success : false, message : 'No token provided'});
+        
+    }
+    
+    
 });
 
+
+/*
 function encrypt(plainText, salt) {
     User.findOne( {email : req.body.email}, function(err, user) {
         console.log('User created this is your unique user ID: ' + user._id);
         res.send('User created ' + ' your username is: ' + user.userName + ' and this is your unique user ID: ' + user._id);
     });
 });
-
+*/
 
 // function encrypt(plainText, salt) {
 //     var algorithm = 'aes-256-ctr';
@@ -144,6 +174,15 @@ function encrypt(plainText, salt) {
 //     dec += decipher.final('utf8');
 //     return dec;
 // }
+
+function generateHash(password, salt) {
+    var hash = crypto
+            .createHash('sha256')
+            .update(salt + password)
+            .digest('base64');
+    
+    return hash;
+}
 
 function generateSalt() {
    return crypto.randomBytes(16).toString('base64');
